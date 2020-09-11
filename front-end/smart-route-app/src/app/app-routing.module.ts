123ac5a337ee438eda30640551e8dbc2f3b76068
marcmerih <merih.atasoy@gmail.com>
2020-09-11 12:35:56 -0400

    created initial accounts page

--- front-end/smart-route-app/src/app/app-routing.module.ts
+++ front-end/smart-route-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'accounts',
+    component: AccountsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/smart-route-app/src/app/app.module.ts
+++ front-end/smart-route-app/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountsComponent } from './accounts/accounts.component';
+import { MatButtonToggleModule } from '@angular/material/button-toggle'
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+
+    // Angular Material
+    MatButtonToggleModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
