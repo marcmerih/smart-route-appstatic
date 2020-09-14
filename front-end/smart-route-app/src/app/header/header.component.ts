d9bbf36503c7fb8d602561553bb48f2bdc8fb31d
mciserani <matteo.ciserani@mail.utoronto.ca>
2020-09-14 16:11:24 +0200

    monday morning commit

---
+++ front-end/smart-route-app/src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Route } from '@angular/compiler/src/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  is_menu_displayed = false;
+
+  constructor(private router: Router ) {}
+
+  ngOnInit(): void {
+  }
+  toggle_menu_display() {
+    this.is_menu_displayed = !this.is_menu_displayed ;
+  }
+  navigate_to_accounts() {
+    this.router.navigateByUrl('/accounts');
+  }
+} 
