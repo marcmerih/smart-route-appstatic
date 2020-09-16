1edcc4307a88d27e0d120a681dbafe315f76aeb4
marcmerih <merih.atasoy@gmail.com>
2020-09-16 17:57:49 -0400

    added ol

--- front-end/smart-route-app/src/app/routes/routes.component.ts
+++ front-end/smart-route-app/src/app/routes/routes.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {  } from '../../../node_modules/ol'
+import { defaults as defaultControls } from 'ol/control';
 
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+import ZoomToExtent from 'ol/control/ZoomToExtent';
 @Component({
   selector: 'app-routes',
   templateUrl: './routes.component.html',
   styleUrls: ['./routes.component.scss']
 })
-export class RoutesComponent implements OnInit {
+export class RoutesComponent implements AfterViewInit {
+  map: Map;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: [813079.7791264898, 5929220.284081122],
+        zoom: 7
+      }),
+      controls: defaultControls().extend([
+        new ZoomToExtent({
+          extent: [
+            813079.7791264898, 5929220.284081122,
+            848966.9639063801, 5936863.986909639
+          ]
+        })
+      ])
+    });
   }
 
 }
